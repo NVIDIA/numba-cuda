2385c3738d57073fd77fe15e5c34b72cfa72ee9a
Graham Markall <gmarkall@nvidia.com>
2024-06-05 19:57:47 +0100

    Fix test discovery
    The modified `load_testsuite()` function accounts for differences in the
    structure of this CUDA target and the upstream Numba repository.

--- numba_cuda/numba/cuda/tests/__init__.py
+++ numba_cuda/numba/cuda/tests/__init__.py
@@ -1,8 +1,41 @@
+from fnmatch import fnmatch
 from numba.cuda.testing import ensure_supported_ccs_initialized
 from numba.testing import unittest
-from numba.testing import load_testsuite
 from numba import cuda
-from os.path import dirname, join
+from os.path import dirname, isfile, join, normpath, relpath, splitext
+
+import os
+import sys
+import traceback
+
+
+# Copied and modified from numba/testing/__init__.py, to handle the difference
+# between the top dirs for Numba and the CUDA target
+def load_testsuite(loader, dir):
+    """Find tests in 'dir'."""
+    top_level_dir = dirname(dirname(dirname(dirname(__file__))))
+    try:
+        suite = unittest.TestSuite()
+        files = []
+        for f in os.listdir(dir):
+            path = join(dir, f)
+            if isfile(path) and fnmatch(f, 'test_*.py'):
+                files.append(f)
+            elif isfile(join(path, '__init__.py')):
+                suite.addTests(loader.discover(path,
+                                               top_level_dir=top_level_dir))
+        for f in files:
+            # turn 'f' into a filename relative to the toplevel dir and
+            # translate it to a module name. This differs from the
+            # implementation in Numba, because the toplevel dir is the
+            # numba_cuda module location, not the numba one.
+            f = relpath(join(dir, f), top_level_dir)
+            f = splitext(normpath(f.replace(os.path.sep, '.')))[0]
+            suite.addTests(loader.loadTestsFromName(f))
+        return suite
+    except Exception:
+        traceback.print_exc(file=sys.stderr)
+        sys.exit(-1)
 
 
 def load_tests(loader, tests, pattern):
@@ -16,7 +49,8 @@ def load_tests(loader, tests, pattern):
         if gpus and gpus[0].compute_capability >= (2, 0):
             suite.addTests(load_testsuite(loader, join(this_dir, 'cudadrv')))
             suite.addTests(load_testsuite(loader, join(this_dir, 'cudapy')))
-            suite.addTests(load_testsuite(loader, join(this_dir, 'doc_examples')))
+            suite.addTests(load_testsuite(loader, join(this_dir,
+                                                       'doc_examples')))
         else:
             print("skipped CUDA tests because GPU CC < 2.0")
     else:
