216cb2d855bcaa54c299fc6b5e823a89d2298be2
Graham Markall <gmarkall@nvidia.com>
2024-06-13 14:44:18 +0100

    Use NVIDIA docs theme and green Numba favicon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,21 +7,36 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Numba CUDA'
-copyright = '2024, NVIDIA'
-author = 'NVIDIA'
+copyright = '2012-2024 Anaconda Inc. 2024, NVIDIA Corporation.'
+author = 'NVIDIA Corporation'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['numpydoc', 'sphinx.ext.intersphinx']
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'llvmlite': ('https://llvmlite.readthedocs.io/en/latest/', None),
+    'numba': ('https://numba.readthedocs.io/en/latest/', None),
+}
 
+# To prevent autosummary warnings
+numpydoc_show_class_members = False
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+try:
+    import nvidia_sphinx_theme  # noqa: F401
+    html_theme = "nvidia_sphinx_theme"
+except ImportError:
+    html_theme = "sphinx_rtd_theme"
+
 html_static_path = ['_static']
+html_favicon = "_static/numba-green-icon-rgb.svg"
+html_show_sphinx = False
