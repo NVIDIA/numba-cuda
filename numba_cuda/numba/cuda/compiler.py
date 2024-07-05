0a08d6ba59241238fb7dbcf56bb4bf2ab0326077
dlee992 <lidanuaa@gmail.com>
2024-07-05 16:18:43 -0500

    handle `cuda.compile` in the same way

--- numba_cuda/numba/cuda/compiler.py
+++ numba_cuda/numba/cuda/compiler.py
@@ -254,7 +254,7 @@ def cabi_wrap_function(context, lib, fndesc, wrapper_function_name,
 
 @global_compiler_lock
 def compile(pyfunc, sig, debug=False, lineinfo=False, device=True,
-            fastmath=False, cc=None, opt=True, abi="c", abi_info=None,
+            fastmath=False, cc=None, opt=None, abi="c", abi_info=None,
             output='ptx'):
     """Compile a Python function to PTX or LTO-IR for a given set of argument
     types.
@@ -283,7 +283,7 @@ def compile(pyfunc, sig, debug=False, lineinfo=False, device=True,
     :param cc: Compute capability to compile for, as a tuple
                ``(MAJOR, MINOR)``. Defaults to ``(5, 0)``.
     :type cc: tuple
-    :param opt: Enable optimizations. Defaults to ``True``.
+    :param opt: Whether to enable optimizations in the compiled code.
     :type opt: bool
     :param abi: The ABI for a compiled function - either ``"numba"`` or
                 ``"c"``. Note that the Numba ABI is not considered stable.
@@ -307,8 +307,11 @@ def compile(pyfunc, sig, debug=False, lineinfo=False, device=True,
     if output not in ("ptx", "ltoir"):
         raise NotImplementedError(f'Unsupported output type: {output}')
 
+    debug = config.CUDA_DEBUGINFO_DEFAULT if debug is None else debug
+    opt = (config.OPT != 0) if opt is None else opt
+
     if debug and opt:
-        msg = ("debug=True with opt=True (the default) "
+        msg = ("debug=True with opt=True "
                "is not supported by CUDA. This may result in a crash"
                " - set debug=False or opt=False.")
         warn(NumbaInvalidConfigWarning(msg))
