bcb9b90e392b9914acf6e23fc3f6fb7b06a0f6d3
Graham Markall <gmarkall@nvidia.com>
2024-07-16 23:52:37 +0100

    Refactor copy_redirector_file()

--- setup.py
+++ setup.py
@@ -9,16 +9,15 @@ from setuptools.command.build_py import build_py
 class build_py_with_redirector(build_py):  # noqa: N801
     """Include the redirector files in the generated wheel."""
 
-    def copy_redirector_file(self, extension):
-        redirector = f"_numba_cuda_redirector.{extension}"
-        source = pathlib.Path("site-packages") / redirector
-        destination = pathlib.Path(self.build_lib) / redirector
+    def copy_redirector_file(self, source, destination="."):
+        destination = pathlib.Path(self.build_lib) / destination
         self.copy_file(str(source), str(destination), preserve_mode=0)
 
     def run(self):
         super().run()
-        self.copy_redirector_file("pth")
-        self.copy_redirector_file("py")
+        site_packages = pathlib.Path("site-packages")
+        self.copy_redirector_file(site_packages / "_numba_cuda_redirector.pth")
+        self.copy_redirector_file(site_packages / "_numba_cuda_redirector.py")
 
 
 setup(cmdclass={"build_py": build_py_with_redirector})
