c19dc41277d71b594f53f674c230a776315899fc
Graham Markall <gmarkall@nvidia.com>
2024-07-24 17:53:14 +0100

    setup.py: Add redirector to source and output mapping
    This corrects the issue of missing redirector files in the sdist.

--- setup.py
+++ setup.py
@@ -19,5 +19,23 @@ class build_py_with_redirector(build_py):  # noqa: N801
         self.copy_redirector_file(site_packages / "_numba_cuda_redirector.pth")
         self.copy_redirector_file(site_packages / "_numba_cuda_redirector.py")
 
+    def get_source_files(self):
+        src = super().get_source_files()
+        site_packages = pathlib.Path("site-packages")
+        src.extend([
+            str(site_packages / "_numba_cuda_redirector.pth"),
+            str(site_packages / "_numba_cuda_redirector.py"),
+        ])
+        return src
+
+    def get_output_mapping(self):
+        mapping = super().get_output_mapping()
+        build_lib = pathlib.Path(self.build_lib)
+        mapping[str(build_lib / "_numba_cuda_redirector.pth")] = \
+            "_numba_cuda_redirector.pth"
+        mapping[str(build_lib / "_numba_cuda_redirector.py")] = \
+            "_numba_cuda_redirector.py"
+        return mapping
+
 
 setup(cmdclass={"build_py": build_py_with_redirector})
