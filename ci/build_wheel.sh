b809f3ae65517c75d104a7e5089e9fbd3aab8e42
Graham Markall <gmarkall@nvidia.com>
2024-09-16 10:38:50 +0100

    Build and upload sdist
    Switching to using the `build` module from `pip` to build the sdist and
    to build the wheel from the sdist - this ensures that a wheel is
    buildable from the sdist. The use of the `wheel` module to build the
    wheel was inherited from other scripts in RAPIDS that were copied, and I
    don't think there was any reason to prefer it over the `build` module in
    the context of numba-cuda.
    
    CI scripts have been updated accordingly to also upload the sdist (so it
    can be inspected if necessary). The `gh-action-pypi-publish` step is
    unchanged as it is expected to pick up the sdist from the `dist/`
    directory.
    
    The version is bumped to 0.0.15 so that a new version with the sdist can
    be published to PyPI once it is merged and tagged.

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -3,10 +3,16 @@
 
 set -euo pipefail
 
-rapids-logger "Build wheel"
-mkdir -p ./dist
-python -m pip wheel . --wheel-dir=./dist -vvv --disable-pip-version-check --no-deps
+rapids-logger "Install build package"
+python -m pip install build
 
-package_path=$(realpath ./dist/numba_cuda-*.whl)
-echo "Package path: $package_path"
-echo "package_path=$package_path" >> $GITHUB_ENV
+rapids-logger "Build sdist and wheel"
+python -m build .
+
+wheel_path=$(realpath ./dist/numba_cuda-*.whl)
+echo "Wheel path: $wheel_path"
+echo "wheel_path=$wheel_path" >> $GITHUB_ENV
+
+sdist_path=$(realpath ./dist/numba_cuda-*.tar.gz)
+echo "ssdist path: $sdist_path"
+echo "sdist_path=$sdist_path" >> $GITHUB_ENV
