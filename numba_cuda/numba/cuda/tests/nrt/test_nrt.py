f769cee052ea83a41ee59cac479927a46e949bda
isVoid <isVoid@users.noreply.github.com>
2024-11-27 09:41:00 -0800

    add ptx assertion

--- numba_cuda/numba/cuda/tests/nrt/test_nrt.py
+++ numba_cuda/numba/cuda/tests/nrt/test_nrt.py
@@ -1,3 +1,4 @@
+import re
 import gc
 import numpy as np
 import unittest
@@ -54,6 +55,36 @@ class TestNrtBasic(CUDATestCase):
         with patch('numba.config.CUDA_ENABLE_NRT', True, create=True):
             g[1,1]()
         cuda.synchronize()
+
+    def test_nrt_ptx_contains_refcount(self):
+        @cuda.jit
+        def f(x):
+            return x[:5]
+
+        @cuda.jit
+        def g():
+            x = cuda_empty(10, np.int64)
+            f(x)
+
+        with patch('numba.config.CUDA_ENABLE_NRT', True, create=True):
+            g[1,1]()
+
+        ptx = next(iter(g.inspect_asm().values()))
+
+        # The following checks that a `call` PTX instruction is
+        # emitted for NRT_MemInfo_alloc_aligned, NRT_incref and
+        # NRT_decref
+        p1 = r"call\.uni(.|\n)*NRT_MemInfo_alloc_aligned"
+        match = re.search(p1, ptx)
+        assert match is not None
+
+        p2 = r"call\.uni.*\n.*NRT_incref"
+        match = re.search(p2, ptx)
+        assert match is not None
+
+        p3 = r"call\.uni.*\n.*NRT_decref"
+        match = re.search(p3, ptx)
+        assert match is not None
 
     def test_nrt_returns_correct(self):
         @cuda.jit
