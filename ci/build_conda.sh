6e03ae2fd0bfa4acb1599dac5070aa4bf647c27f
Kyle Edwards <kyedwards@nvidia.com>
2025-03-05 14:03:40 -0500

    s/mambabuild/build/

--- ci/build_conda.sh
+++ ci/build_conda.sh
@@ -13,7 +13,7 @@ rapids-print-env
 
 rapids-logger "Begin py build"
 
-rapids-conda-retry mambabuild conda/recipes/numba-cuda
+rapids-conda-retry build conda/recipes/numba-cuda
 
 package_path=(/tmp/conda-bld-output/noarch/numba-cuda-*.tar.bz2)
 echo "package_path=$package_path" >> $GITHUB_ENV
