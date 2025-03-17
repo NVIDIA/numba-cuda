c6a9b731089c92e163eddf2b3fee787e3862021a
isVoid <isVoid@users.noreply.github.com>
2025-03-17 18:10:53 +0000

    add docstring

--- numba_cuda/numba/cuda/cudadrv/managed_module.py
+++ numba_cuda/numba/cuda/cudadrv/managed_module.py
@@ -5,6 +5,8 @@ from .driver import CtypesModule, USE_NV_BINDING
 
 
 class _CuFuncProxy:
+    """See `ManagedModule` for more details
+    """
     def __init__(self, module, cufunc):
         self._module = module
         self._cufunc = cufunc
@@ -21,6 +23,14 @@ class _CuFuncProxy:
 
 class ManagedModule:
     def __init__(self, module, setup_callbacks, teardown_callbacks):
+        """ctypes module with setup and teardown callbacks
+        The use of managedmodule is the same as a ctypes module,
+        with the exception of `get_function`, which returns a wrapped
+        cufunc object. The wrapped object provides `init_module`
+        and `lazy_finalize_module` method. Which are used
+        to initialize and finalize the module when stream is available
+        in the later stage of the compilation pipeline.
+        """
         # To be updated to object code
         if not isinstance(module, CtypesModule):
             raise TypeError("module must be a CtypesModule")
@@ -43,11 +53,14 @@ class ManagedModule:
         self._teardown_callbacks = teardown_callbacks
 
     def _init(self, stream):
+        """Eagerly call the setup functions for cumodule on `stream`
+        """
         for setup in self._setup_callbacks:
             setup(self._module, stream)
 
     def _lazy_finalize(self, stream):
-
+        """Set teardown function for cumodule via weakref finalizer.
+        """
         def lazy_callback(callbacks, module, stream):
             for teardown in callbacks:
                 teardown(module, stream)
@@ -69,6 +82,8 @@ class ManagedModule:
             )
 
     def get_function(self, name):
+        """Returns wrapped CtypesFunc object.
+        """
         ctypesfunc = self._module.get_function(name)
         return _CuFuncProxy(self, ctypesfunc)
 
