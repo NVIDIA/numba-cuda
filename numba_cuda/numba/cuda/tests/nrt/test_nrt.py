2ed550328dfa2e50643e06e9e79fa6c49d551dba
Graham Markall <gmarkall@nvidia.com>
2025-03-25 15:25:58 +0000

    Rename `numba.cuda.runtime` to `memory_management`
    The name `numba.cuda.runtime` as a package aliased the already-existing
    `Runtime` object. Although the package name is now no longer consistent
    with the package from core Numba containing the Numba Runtime code, it
    needs to be renamed so that we can avoid clashing with already-extant
    CUDA names.
    
    Fixes #174

--- numba_cuda/numba/cuda/tests/nrt/test_nrt.py
+++ numba_cuda/numba/cuda/tests/nrt/test_nrt.py
@@ -8,7 +8,7 @@ from numba.cuda.testing import CUDATestCase
 from numba.tests.support import run_in_subprocess, override_config
 
 from numba import cuda
-from numba.cuda.runtime.nrt import rtsys
+from numba.cuda.memory_management.nrt import rtsys
 
 
 class TestNrtBasic(CUDATestCase):
@@ -94,7 +94,7 @@ class TestNrtStatistics(CUDATestCase):
         # Checks that explicitly turning the stats on via the env var works.
         src = """if 1:
         from numba import cuda
-        from numba.cuda.runtime import rtsys
+        from numba.cuda.memory_management import rtsys
         import numpy as np
 
         @cuda.jit
@@ -133,7 +133,7 @@ class TestNrtStatistics(CUDATestCase):
         src = """if 1:
         from numba import cuda
         import numpy as np
-        from numba.cuda.runtime import rtsys
+        from numba.cuda.memory_management import rtsys
 
         @cuda.jit
         def foo():
