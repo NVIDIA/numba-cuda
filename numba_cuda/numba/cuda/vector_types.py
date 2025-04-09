d236e48d32e54e74d0c3471d51f5fcffbece53a2
Yevhenii Havrylko <yhavrylko@nvidia.com>
2025-04-09 06:17:12 -0700

    Update tests to use cuda specific make_attribute_wrapper

--- numba_cuda/numba/cuda/vector_types.py
+++ numba_cuda/numba/cuda/vector_types.py
@@ -5,13 +5,15 @@ from typing import List, Tuple, Dict
 
 from numba import types
 from numba.core import cgutils
-from numba.core.extending import make_attribute_wrapper, models, register_model
+from numba.core.extending import models
 from numba.core.imputils import Registry as ImplRegistry
 from numba.core.typing.templates import ConcreteTemplate
 from numba.core.typing.templates import Registry as TypingRegistry
 from numba.core.typing.templates import signature
 from numba.cuda import stubs
 from numba.cuda.errors import CudaLoweringError
+from numba.cuda.extending import make_attribute_wrapper, register_model
+
 
 typing_registry = TypingRegistry()
 impl_registry = ImplRegistry()
