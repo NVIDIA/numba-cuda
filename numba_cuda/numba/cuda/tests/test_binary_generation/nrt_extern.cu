a2aa31e0198a2de80d726d2f99f170dd799fe033
brandon-b-miller <brmiller@nvidia.com>
2025-04-21 14:10:50 -0700

    small fix

--- numba_cuda/numba/cuda/tests/test_binary_generation/nrt_extern.cu
+++ numba_cuda/numba/cuda/tests/test_binary_generation/nrt_extern.cu
@@ -1,6 +1,6 @@
 #include <nrt.cuh>
 
-extern "C" __device__ int extern_func(int* nb_retval){
+extern "C" __device__ int device_allocate_deallocate(int* nb_retval){
     auto ptr = NRT_Allocate(1);
     NRT_Free(ptr);
     return 0;
