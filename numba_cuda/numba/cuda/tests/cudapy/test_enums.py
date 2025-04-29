bcaa310ff482fb65a6f9fa9e1a0b5f520673daac
Graham Markall <gmarkall@nvidia.com>
2025-04-29 12:37:50 +0100

    Fix ruff

--- numba_cuda/numba/cuda/tests/cudapy/test_enums.py
+++ numba_cuda/numba/cuda/tests/cudapy/test_enums.py
@@ -6,7 +6,7 @@ import numpy as np
 
 from numba import int16, int32
 from numba import cuda, vectorize, njit
-from numba.core import types, typing
+from numba.core import types
 from numba.cuda.testing import unittest, CUDATestCase, skip_on_cudasim
 from numba.tests.enum_usecases import (
     Color,
