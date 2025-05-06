caf438e2c7d368cb183b0f42ec2cfce180d6de2b
Graham Markall <gmarkall@nvidia.com>
2025-05-06 16:40:15 +0100

    Fix typo in test_extending

--- numba_cuda/numba/cuda/tests/cudapy/test_extending.py
+++ numba_cuda/numba/cuda/tests/cudapy/test_extending.py
@@ -202,7 +202,7 @@ class TestExtendingLinkage(CUDATestCase):
             (test_device_functions_ltoir, cuda.LTOIR),
         )
 
-        if cuda.runtime.get_version[0] < 12:
+        if cuda.runtime.get_version()[0] < 12:
             unittest.skip("CUDA 12 required for linking in-memory data")
 
         lto = config.CUDA_ENABLE_PYNVJITLINK
