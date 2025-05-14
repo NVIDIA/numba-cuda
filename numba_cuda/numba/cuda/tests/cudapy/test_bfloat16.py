2edf5d5646c11753c97e13c793cc9bd6cac4ec27
isVoid <isVoid@users.noreply.github.com>
2025-05-14 21:10:51 +0000

    add test to use bf16 with lto

--- numba_cuda/numba/cuda/tests/cudapy/test_bfloat16.py
+++ numba_cuda/numba/cuda/tests/cudapy/test_bfloat16.py
@@ -15,11 +15,14 @@ class TestBfloat16HighLevelBindings(CUDATestCase):
     def test_use_type_in_kernel(self):
         self.skip_unsupported()
 
-        @cuda.jit
-        def kernel():
-            bfloat16(3.14)
+        for lto in [True, False]:
+            with self.subTest(lto=lto):
 
-        kernel[1, 1]()
+                @cuda.jit(lto=lto)
+                def kernel():
+                    bfloat16(3.14)
+
+                kernel[1, 1]()
 
     def test_math_bindings(self):
         self.skip_unsupported()
@@ -36,20 +39,21 @@ class TestBfloat16HighLevelBindings(CUDATestCase):
             math.exp,
             math.exp2,
         ]
+        for lto in [True, False]:
+            with self.subTest(lto=lto):
+                for f in functions:
+                    with self.subTest(func=f):
 
-        for f in functions:
-            with self.subTest(func=f):
+                        @cuda.jit(lto=lto)
+                        def kernel(arr):
+                            x = bfloat16(3.14)
+                            y = f(x)
+                            arr[0] = float32(y)
 
-                @cuda.jit
-                def kernel(arr):
-                    x = bfloat16(3.14)
-                    y = f(x)
-                    arr[0] = float32(y)
+                        arr = cuda.device_array((1,), dtype="float32")
+                        kernel[1, 1](arr)
 
-                arr = cuda.device_array((1,), dtype="float32")
-                kernel[1, 1](arr)
-
-                if f in (math.exp, math.exp2):
-                    self.assertAlmostEqual(arr[0], f(3.14), delta=1e-1)
-                else:
-                    self.assertAlmostEqual(arr[0], f(3.14), delta=1e-2)
+                        if f in (math.exp, math.exp2):
+                            self.assertAlmostEqual(arr[0], f(3.14), delta=1e-1)
+                        else:
+                            self.assertAlmostEqual(arr[0], f(3.14), delta=1e-2)
