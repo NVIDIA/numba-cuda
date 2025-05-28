394643254cf60512f57180cddd3a51ba3be9dd62
jiel-nv <jiel@nvidia.com>
2025-05-28 13:10:18 -0700

    Add debug support for dynamic types
    This change implements debugging support for polymorphic variables via a C Union
    approach.
    
    (1) A pre-lower scan on function body checks all the assignment instructions for
    versioned / unversioned  targets names and generates a polymorphic types map for
    each qualified source name;
    (2) Frond end Literal types are deduplicated before recorded into an entry.
    (3) Leverage the UnionType and UnionModel to create and handle the aggreate type
    where the "payload" of field representing data types in memory.
    (4) The allccation of memory uses a single element aggregate type and necessary
    type casts are made for easy retrieval of member fields of the artificial union.
    
    This fulfills the RFE in nvbug#5267572.

--- numba_cuda/numba/cuda/lowering.py
+++ numba_cuda/numba/cuda/lowering.py
@@ -1,6 +1,7 @@
 from numba.core.lowering import Lower
 from llvmlite import ir
-
+from numba.core import ir as numba_ir
+from numba.core import types
 
 class CUDALower(Lower):
     def storevar(self, value, name, argidx=None):
@@ -14,10 +15,7 @@ class CUDALower(Lower):
         if (
             self.context.enable_debuginfo
             # Conditions used to elide stores in parent method
-            and (
-                name not in self._singly_assigned_vars
-                or self._disable_sroa_like_opt
-            )
+            and self.store_var_needed(name)
             # No emission of debuginfo for internal names
             and not name.startswith("$")
         ):
@@ -27,6 +25,11 @@ class CUDALower(Lower):
             int_type = (ir.IntType,)
             real_type = ir.FloatType, ir.DoubleType
             if isinstance(lltype, int_type + real_type):
+                index = name.find(".")
+                src_name = name[:index] if index > 0 else name
+                if src_name in self.poly_var_typ_map:
+                    # Do not emit debug value on polymorphic type var
+                    return
                 # Emit debug value for scalar variable
                 sizeof = self.context.get_abi_sizeof(lltype)
                 datamodel = self.context.data_model_manager[fetype]
@@ -41,3 +44,72 @@ class CUDALower(Lower):
                     datamodel,
                     argidx,
                 )
+
+    def pre_lower(self):
+        """
+        Called before lowering all blocks.
+        """
+        super().pre_lower()
+
+        self.poly_var_typ_map = {}
+        self.poly_var_loc_map = {}
+
+        # When debug info is enabled, walk through function body and mark
+        # variables with polymorphic types.
+        if self.context.enable_debuginfo and self._disable_sroa_like_opt:
+            # pre-scan all blocks
+            for block in self.blocks.values():
+                for x in block.find_insts(numba_ir.Assign):
+                    if x.target.name.startswith("$"):
+                        continue
+                    ssa_name = x.target.name
+                    index = ssa_name.find(".")
+                    src_name = ssa_name[:index] if index > 0 else ssa_name
+                    if len(x.target.versioned_names) > 0:
+                        fetype = self.typeof(ssa_name)
+                        if src_name not in self.poly_var_typ_map:
+                            self.poly_var_typ_map[src_name] = set()
+                        # deduplicate polymorphic types
+                        if isinstance(fetype, types.Literal):
+                            fetype = fetype.literal_type
+                        self.poly_var_typ_map[src_name].add(fetype)
+
+    def _alloca_var(self, name, fetype):
+        """
+        Ensure the given variable has an allocated stack slot (if needed).
+        """
+        #super()._alloca_var(name, fetype)
+        # If the name is not handled yet and a store is needed
+        if (name not in self.varmap and self.store_var_needed(name)):
+            index = name.find(".")
+            src_name = name[:index] if index > 0 else name
+            if src_name in self.poly_var_typ_map:
+                dtype = types.UnionType(self.poly_var_typ_map[src_name])
+                datamodel = self.context.data_model_manager[dtype]
+                if src_name not in self.poly_var_loc_map:
+                    # UnionType has sorted set of types, max at last index
+                    maxsizetype = dtype.types[-1]
+                    # Create a single element aggregate type
+                    aggr_type = types.UniTuple(maxsizetype, 1)
+                    lltype = self.context.get_value_type(aggr_type)
+                    ptr = self.alloca_lltype(src_name, lltype, datamodel)
+                    # save the location of the union type for polymorphic var
+                    self.poly_var_loc_map[src_name] = ptr
+                # Any member of this union type shoud type cast ptr to fetype
+                lltype = self.context.get_value_type(fetype)
+                castptr = self.builder.bitcast(self.poly_var_loc_map[src_name],
+                                               ir.PointerType(lltype))
+                # Remember the pointer
+                self.varmap[name] = castptr
+
+        super()._alloca_var(name, fetype)
+
+    def store_var_needed(self, name):
+        # Check the conditions used to elide stores in parent class,
+        # e.g. in method storevar() and _alloca_var()
+        return (
+            # used in multiple blocks
+            name not in self._singly_assigned_vars
+            # lowering with debuginfo
+            or self._disable_sroa_like_opt
+        )
