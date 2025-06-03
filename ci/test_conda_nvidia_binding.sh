a98ded0adda0e872d741b9e7d3964e7f3f163c33
brandon-b-miller <brmiller@nvidia.com>
2025-06-03 09:00:09 -0700

    missed env var

--- ci/test_conda_nvidia_binding.sh
+++ ci/test_conda_nvidia_binding.sh
@@ -64,7 +64,7 @@ popd
 
 
 rapids-logger "Run Tests"
-NUMBA_CUDA_ENABLE_PYNVJITLINK=1 NUMBA_CUDA_TEST_BIN_DIR=$NUMBA_CUDA_TEST_BIN_DIR python -m numba.runtests numba.cuda.tests -v
+NUMBA_CUDA_USE_NVIDIA_BINDING=1 NUMBA_CUDA_TEST_BIN_DIR=$NUMBA_CUDA_TEST_BIN_DIR python -m numba.runtests numba.cuda.tests -v
 
 popd
 
