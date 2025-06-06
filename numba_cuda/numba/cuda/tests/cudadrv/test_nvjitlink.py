72d34d72f403cb3df92e23ad998ddb39d768a75b
brandon-b-miller <brmiller@nvidia.com>
2025-06-06 08:06:59 -0700

    prune nvjitlink tests

--- numba_cuda/numba/cuda/tests/cudadrv/test_nvjitlink.py
+++ numba_cuda/numba/cuda/tests/cudadrv/test_nvjitlink.py
@@ -1,21 +1,12 @@
 from numba.cuda.testing import unittest
 from numba.cuda.testing import skip_on_cudasim
 from numba.cuda.testing import CUDATestCase
-from numba.cuda.cudadrv.driver import PyNvJitLinker
 from numba.cuda import get_current_device
+from numba.cuda.cudadrv.driver import _Linker
 
 from numba import cuda
 from numba import config
-from numba.tests.support import run_in_subprocess, override_config
 
-try:
-    import pynvjitlink  # noqa: F401
-
-    PYNVJITLINK_INSTALLED = True
-except ImportError:
-    PYNVJITLINK_INSTALLED = False
-
-import itertools
 import os
 import io
 import contextlib
@@ -51,87 +42,11 @@ if TEST_BIN_DIR:
 
 
 @unittest.skipIf(
-    not config.CUDA_ENABLE_PYNVJITLINK or not TEST_BIN_DIR,
-    "pynvjitlink not enabled",
+    not config.CUDA_USE_NVIDIA_BINDING or not TEST_BIN_DIR,
+    "NVIDIA cuda bindings not enabled",
 )
 @skip_on_cudasim("Linking unsupported in the simulator")
 class TestLinker(CUDATestCase):
-    _NUMBA_NVIDIA_BINDING_0_ENV = {"NUMBA_CUDA_USE_NVIDIA_BINDING": "0"}
-
-    def test_nvjitlink_create(self):
-        patched_linker = PyNvJitLinker(cc=(7, 5))
-        assert "-arch=sm_75" in patched_linker.options
-
-    def test_nvjitlink_create_no_cc_error(self):
-        # nvJitLink expects at least the architecture to be specified.
-        with self.assertRaisesRegex(
-            RuntimeError, "PyNvJitLinker requires CC to be specified"
-        ):
-            PyNvJitLinker()
-
-    def test_nvjitlink_invalid_arch_error(self):
-        from pynvjitlink.api import NvJitLinkError
-
-        # CC 0.0 is not a valid compute capability
-        with self.assertRaisesRegex(
-            NvJitLinkError, "NVJITLINK_ERROR_UNRECOGNIZED_OPTION error"
-        ):
-            PyNvJitLinker(cc=(0, 0))
-
-    def test_nvjitlink_invalid_cc_type_error(self):
-        with self.assertRaisesRegex(
-            TypeError, "`cc` must be a list or tuple of length 2"
-        ):
-            PyNvJitLinker(cc=0)
-
-    def test_nvjitlink_ptx_compile_options(self):
-        max_registers = (None, 32)
-        lineinfo = (False, True)
-        lto = (False, True)
-        additional_flags = (None, ("-g",), ("-g", "-time"))
-        for (
-            max_registers_i,
-            line_info_i,
-            lto_i,
-            additional_flags_i,
-        ) in itertools.product(max_registers, lineinfo, lto, additional_flags):
-            with self.subTest(
-                max_registers=max_registers_i,
-                lineinfo=line_info_i,
-                lto=lto_i,
-                additional_flags=additional_flags_i,
-            ):
-                patched_linker = PyNvJitLinker(
-                    cc=(7, 5),
-                    max_registers=max_registers_i,
-                    lineinfo=line_info_i,
-                    lto=lto_i,
-                    additional_flags=additional_flags_i,
-                )
-                assert "-arch=sm_75" in patched_linker.options
-
-                if max_registers_i:
-                    assert (
-                        f"-maxrregcount={max_registers_i}"
-                        in patched_linker.options
-                    )
-                else:
-                    assert "-maxrregcount" not in patched_linker.options
-
-                if line_info_i:
-                    assert "-lineinfo" in patched_linker.options
-                else:
-                    assert "-lineinfo" not in patched_linker.options
-
-                if lto_i:
-                    assert "-lto" in patched_linker.options
-                else:
-                    assert "-lto" not in patched_linker.options
-
-                if additional_flags_i:
-                    for flag in additional_flags_i:
-                        assert flag in patched_linker.options
-
     def test_nvjitlink_add_file_guess_ext_linkable_code(self):
         files = (
             test_device_functions_a,
@@ -143,24 +58,20 @@ class TestLinker(CUDATestCase):
         )
         for file in files:
             with self.subTest(file=file):
-                patched_linker = PyNvJitLinker(
-                    cc=get_current_device().compute_capability
-                )
-                patched_linker.add_file_guess_ext(file)
+                linker = _Linker(cc=get_current_device().compute_capability)
+                linker.add_file_guess_ext(file)
 
     def test_nvjitlink_test_add_file_guess_ext_invalid_input(self):
         with open(test_device_functions_cubin, "rb") as f:
             content = f.read()
 
-        patched_linker = PyNvJitLinker(
-            cc=get_current_device().compute_capability
-        )
+        linker = _Linker(cc=get_current_device().compute_capability)
         with self.assertRaisesRegex(
             TypeError, "Expected path to file or a LinkableCode"
         ):
             # Feeding raw data as bytes to add_file_guess_ext should raise,
             # because there's no way to know what kind of file to treat it as
-            patched_linker.add_file_guess_ext(content)
+            linker.add_file_guess_ext(content)
 
     def test_nvjitlink_jit_with_linkable_code(self):
         files = (
@@ -260,78 +171,6 @@ class TestLinker(CUDATestCase):
             @cuda.jit("void()", link=[content])
             def kernel():
                 pass
-
-
-@unittest.skipIf(
-    not PYNVJITLINK_INSTALLED or not TEST_BIN_DIR,
-    reason="pynvjitlink not enabled",
-)
-@skip_on_cudasim("Linking unsupported in the simulator")
-class TestLinkerUsage(CUDATestCase):
-    """Test that whether pynvjitlink can be enabled by both environment variable
-    and modification of config at runtime.
-    """
-
-    src = """if 1:
-        import os
-        from numba import cuda, config
-
-        {config}
-
-        TEST_BIN_DIR = os.getenv("NUMBA_CUDA_TEST_BIN_DIR")
-        if TEST_BIN_DIR:
-            test_device_functions_cubin = os.path.join(
-                TEST_BIN_DIR, "test_device_functions.cubin"
-            )
-
-        sig = "uint32(uint32, uint32)"
-        add_from_numba = cuda.declare_device("add_from_numba", sig)
-
-        @cuda.jit(link=[test_device_functions_cubin], lto=True)
-        def kernel(result):
-            result[0] = add_from_numba(1, 2)
-
-        result = cuda.device_array(1)
-        kernel[1, 1](result)
-        assert result[0] == 3
-        """
-
-    def test_linker_enabled_envvar(self):
-        env = os.environ.copy()
-        env.pop("NUMBA_CUDA_ENABLE_PYNVJITLINK", None)
-        run_in_subprocess(self.src.format(config=""), env=env)
-
-    def test_linker_disabled_envvar(self):
-        env = os.environ.copy()
-        env["NUMBA_CUDA_ENABLE_PYNVJITLINK"] = "0"
-        with self.assertRaisesRegex(
-            AssertionError, "LTO and additional flags require PyNvJitLinker"
-        ):
-            # Actual error raised is `ValueError`, but `run_in_subprocess`
-            # reraises as AssertionError.
-            run_in_subprocess(self.src.format(config=""), env=env)
-
-    def test_linker_enabled_config(self):
-        env = os.environ.copy()
-        env.pop("NUMBA_CUDA_ENABLE_PYNVJITLINK", None)
-        run_in_subprocess(
-            self.src.format(config="config.CUDA_ENABLE_PYNVJITLINK = True"),
-            env=env,
-        )
-
-    def test_linker_disabled_config(self):
-        env = os.environ.copy()
-        env.pop("NUMBA_CUDA_ENABLE_PYNVJITLINK", None)
-        with override_config("CUDA_ENABLE_PYNVJITLINK", False):
-            with self.assertRaisesRegex(
-                AssertionError, "LTO and additional flags require PyNvJitLinker"
-            ):
-                run_in_subprocess(
-                    self.src.format(
-                        config="config.CUDA_ENABLE_PYNVJITLINK = False"
-                    ),
-                    env=env,
-                )
 
 
 if __name__ == "__main__":
