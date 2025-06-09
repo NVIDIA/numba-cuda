7b72a91842b8d52ff734ff50b85c0158d4840cb2
brandon-b-miller <brmiller@nvidia.com>
2025-06-09 06:20:22 -0700

    use nv binding by default and add dep

--- numba_cuda/numba/cuda/__init__.py
+++ numba_cuda/numba/cuda/__init__.py
@@ -1,18 +1,23 @@
 from numba import runtests
 from numba.core import config
-
-if config.ENABLE_CUDASIM:
-    from .simulator_init import *
-else:
-    from .device_init import *
-    from .device_init import _auto_device
-
 from numba.cuda.compiler import (
     compile,
     compile_for_current_device,
     compile_ptx,
     compile_ptx_for_current_device,
 )
+from .utils import _readenv
+
+
+config.CUDA_USE_NVIDIA_BINDING = _readenv(
+    "NUMBA_CUDA_USE_NVIDIA_BINDING", bool, True
+)
+if config.ENABLE_CUDASIM:
+    from .simulator_init import *
+else:
+    from .device_init import *
+    from .device_init import _auto_device
+
 
 # This is the out-of-tree NVIDIA-maintained target. This is reported in Numba
 # sysinfo (`numba -s`):
