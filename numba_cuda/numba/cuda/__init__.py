e607304c72bf4e43a877ff6164065ac27433edbd
brandon-b-miller <brmiller@nvidia.com>
2025-06-09 09:42:18 -0700

    reset file

--- numba_cuda/numba/cuda/__init__.py
+++ numba_cuda/numba/cuda/__init__.py
@@ -1,14 +1,5 @@
-from numba.core import config
-
 from numba import runtests
-from numba.cuda.compiler import (
-    compile,
-    compile_for_current_device,
-    compile_ptx,
-    compile_ptx_for_current_device,
-)
-from .utils import _readenv
-
+from numba.core import config
 
 if config.ENABLE_CUDASIM:
     from .simulator_init import *
@@ -16,6 +7,12 @@ else:
     from .device_init import *
     from .device_init import _auto_device
 
+from numba.cuda.compiler import (
+    compile,
+    compile_for_current_device,
+    compile_ptx,
+    compile_ptx_for_current_device,
+)
 
 # This is the out-of-tree NVIDIA-maintained target. This is reported in Numba
 # sysinfo (`numba -s`):
