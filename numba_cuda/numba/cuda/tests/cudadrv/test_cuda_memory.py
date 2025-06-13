7a94cfc3eca7f64c26e169dcdbbb0a942f577796
Graham Markall <535640+gmarkall@users.noreply.github.com>
2025-06-13 22:10:15 +0100

    Fix `DeviceNDArray.device_ctypes_pointer` to always be ctypes (#295)
    When the NVIDIA bindings are in use, `device_ctypes_pointer` should
    always be a ctypes pointer, otherwise the interface changes depending on
    which set of bindings are in use. This has been a long-standing
    inconsistency, but it needs to be resolved.
    
    The fundamental change is only to `DeviceNDArray.device_ctypes_pointer`.
    The associated changes are all fixups to maintain internal consistency.

--- numba_cuda/numba/cuda/tests/cudadrv/test_cuda_memory.py
+++ numba_cuda/numba/cuda/tests/cudadrv/test_cuda_memory.py
@@ -20,10 +20,7 @@ class TestCudaMemory(ContextResettingTestCase):
     def _template(self, obj):
         self.assertTrue(driver.is_device_memory(obj))
         driver.require_device_memory(obj)
-        if driver.USE_NV_BINDING:
-            expected_class = driver.binding.CUdeviceptr
-        else:
-            expected_class = drvapi.cu_device_ptr
+        expected_class = drvapi.cu_device_ptr
         self.assertTrue(isinstance(obj.device_ctypes_pointer, expected_class))
 
     def test_device_memory(self):
