a0eabdd18074c48a49334032e6dd025753c3943e
brandon-b-miller <brmiller@nvidia.com>
2025-06-13 12:53:45 -0700

    fix simulator

--- numba_cuda/numba/cuda/simulator/cudadrv/driver.py
+++ numba_cuda/numba/cuda/simulator/cudadrv/driver.py
@@ -34,10 +34,10 @@ class FakeDriver(object):
 driver = FakeDriver()
 
 
-class Linker:
+class _Linker:
     @classmethod
     def new(cls, max_registers=0, lineinfo=False, cc=None):
-        return Linker()
+        return _Linker()
 
     @property
     def lto(self):
