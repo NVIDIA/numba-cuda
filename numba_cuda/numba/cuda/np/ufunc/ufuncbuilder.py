a268f335a2666a3c6e3a0d9787666bd15c1d1702
Atmn Patel <atmnp@nvidia.com>
2025-08-08 10:15:08 -0700

    [Refactor][NFC] Vendor in ufunc utility functions, classes

---
+++ numba_cuda/numba/cuda/np/ufunc/ufuncbuilder.py
@@ -0,0 +1,56 @@
+from numba.cuda.core import sigutils
+from numba.np.ufunc import _internal
+
+# Utility functions
+
+
+def _compile_element_wise_function(nb_func, targetoptions, sig):
+    # Do compilation
+    # Return CompileResult to test
+    cres = nb_func.compile(sig, **targetoptions)
+    args, return_type = sigutils.normalize_signature(sig)
+    return cres, args, return_type
+
+
+# Class definitions
+
+
+class _BaseUFuncBuilder(object):
+    def add(self, sig=None):
+        if hasattr(self, "targetoptions"):
+            targetoptions = self.targetoptions
+        else:
+            targetoptions = self.nb_func.targetoptions
+        cres, args, return_type = _compile_element_wise_function(
+            self.nb_func, targetoptions, sig
+        )
+        sig = self._finalize_signature(cres, args, return_type)
+        self._sigs.append(sig)
+        self._cres[sig] = cres
+        return cres
+
+    def disable_compile(self):
+        """
+        Disable the compilation of new signatures at call time.
+        """
+        # Override this for implementations that support lazy compilation
+
+
+_identities = {
+    0: _internal.PyUFunc_Zero,
+    1: _internal.PyUFunc_One,
+    None: _internal.PyUFunc_None,
+    "reorderable": _internal.PyUFunc_ReorderableNone,
+}
+
+
+def parse_identity(identity):
+    """
+    Parse an identity value and return the corresponding low-level value
+    for Numpy.
+    """
+    try:
+        identity = _identities[identity]
+    except KeyError:
+        raise ValueError("Invalid identity value %r" % (identity,))
+    return identity
