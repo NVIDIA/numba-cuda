a2a662bf07b1fedac64a6069f769a649c082979b
brandon-b-miller <brmiller@nvidia.com>
2025-08-12 08:31:38 -0700

    cudf 25.06 doesnt have its own libnvjitlink requirememnt yet

--- ci/test_thirdparty.sh
+++ ci/test_thirdparty.sh
@@ -21,6 +21,7 @@ python -m pip install \
     "${package}[test]" \
     "cuda-python==${CUDA_VER_MAJOR_MINOR%.*}.*" \
     "cuda-core==0.3.*" \
+    "nvidia-nvjitlink-cu12" \ # branch-25.06 in cuDF too early to require
 
 
 rapids-logger "Shallow clone cuDF repository"
