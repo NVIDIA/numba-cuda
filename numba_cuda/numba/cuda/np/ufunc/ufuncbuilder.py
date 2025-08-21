a7c847e7ec6eee69e3a67c1edf58d3d2f6f90ebf
Atmn Patel <atmnp@nvidia.com>
2025-08-21 15:26:52 -0700

    Adds SPDX identifier to newly added ufuncbuilder.py

--- numba_cuda/numba/cuda/np/ufunc/ufuncbuilder.py
+++ numba_cuda/numba/cuda/np/ufunc/ufuncbuilder.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
 from numba.cuda.core import sigutils
 from numba.np.ufunc import _internal
 
