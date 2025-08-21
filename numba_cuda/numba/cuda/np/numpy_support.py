e3288e240623509382676ffbedb0f082cb36952f
Atmn Patel <atmnp@nvidia.com>
2025-08-21 08:08:38 -0700

    Adds SPDX identifier to newly added numpy_support.py

--- numba_cuda/numba/cuda/np/numpy_support.py
+++ numba_cuda/numba/cuda/np/numpy_support.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
 import numpy as np
 import re
 from numba.core import types, errors, config
