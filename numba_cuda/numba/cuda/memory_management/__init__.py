7befd396f6f0b6545135095d33c05a8e1a85a3fa
Graham Markall <gmarkall@nvidia.com>
2025-08-26 17:59:10 +0100

    Add an `Array._allocate` overload for the CUDA target

--- numba_cuda/numba/cuda/memory_management/__init__.py
+++ numba_cuda/numba/cuda/memory_management/__init__.py
@@ -2,3 +2,7 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
 from numba.cuda.memory_management.nrt import rtsys  # noqa: F401
+from numba.cuda.memory_management import arrayobj_extras  # noqa: F401
+
+
+__all__ = ["rtsys"]
