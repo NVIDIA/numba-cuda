67759e91d44b7719c9bec9d4c76703486ee4d72e
Atmn Patel <atmnp@nvidia.com>
2025-08-27 13:57:52 -0700

    [Refactor][NFC] Vendor-in core Numba analysis utils for CUDA-specific changes

--- numba_cuda/numba/cuda/core/ssa.py
+++ numba_cuda/numba/cuda/core/ssa.py
@@ -20,7 +20,7 @@ from collections import defaultdict
 from numba import config
 from numba.core import ir, ir_utils, errors
 from numba.cuda.utils import OrderedSet, _lazy_pformat
-from numba.core.analysis import compute_cfg_from_blocks
+from numba.cuda.core.analysis import compute_cfg_from_blocks
 
 
 _logger = logging.getLogger(__name__)
--- numba_cuda/numba/cuda/lowering.py
+++ numba_cuda/numba/cuda/lowering.py
@@ -28,7 +28,7 @@ from numba.core.errors import (
 )
 from numba.cuda.core.funcdesc import default_mangler
 from numba.cuda.core.environment import Environment
-from numba.core.analysis import compute_use_defs, must_use_alloca
+from numba.cuda.core.analysis import compute_use_defs, must_use_alloca
 from numba.misc.firstlinefinder import get_func_body_first_lineno
 from numba import version_info
 
