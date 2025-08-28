09be92f5e82977ccb1722244dfda87441810df8f
Vijay Kandiah <vkandiah@nvidia.com>
2025-08-28 09:49:35 -0700

    Add SPDX license to new files

--- numba_cuda/numba/cuda/typing/cffi_utils.py
+++ numba_cuda/numba/cuda/typing/cffi_utils.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
 """
 Support for CFFI. Allows checking whether objects are CFFI functions and
 obtaining the pointer and numba signature.
