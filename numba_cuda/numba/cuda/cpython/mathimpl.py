38a84c098b9249b2bc35916ebca9e1c13d4fa80e
Asher Mancinelli <ashermancinelli@gmail.com>
2025-08-29 12:32:07 -0700

    Fix imports

--- numba_cuda/numba/cuda/cpython/mathimpl.py
+++ numba_cuda/numba/cuda/cpython/mathimpl.py
@@ -13,7 +13,7 @@ import numpy as np
 import llvmlite.ir
 from llvmlite.ir import Constant
 
-from numba.core.imputils import impl_ret_untracked, Registry
+from numba.cuda.core.imputils import impl_ret_untracked, Registry
 from numba.core import types, config
 from numba.core.extending import overload
 from numba.core.typing import signature
