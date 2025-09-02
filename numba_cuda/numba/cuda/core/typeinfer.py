c1b3b5c49aee4b21195a653f9d476e6237dbabac
Graham Markall <535640+gmarkall@users.noreply.github.com>
2025-09-02 16:53:02 +0100

    Apply suggestions from code review

--- numba_cuda/numba/cuda/core/typeinfer.py
+++ numba_cuda/numba/cuda/core/typeinfer.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: BSD-2-Clause
 
-#============================================================================#
+# ========================================================================== #
 #                                                                            #
 # IMPORTANT: Before modifying type inference, it is important to port the    #
 #            following tests from `numba.tests.test_typeinfer`:              #
@@ -12,7 +12,7 @@
 #              - test_list_unify{1,2}: Lists not supported                   #
 #              - test_load_fast_and_clear*: Uses list comprehensions         #
 #                                                                            #
-#============================================================================#
+# ========================================================================== #
 """
 Type inference base on CPA.
 The algorithm guarantees monotonic growth of type-sets for each variable.
