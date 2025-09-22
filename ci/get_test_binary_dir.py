6782a6f348ef52821250d3d5aa412dc5def16198
Rob Parolin <rparolin@nvidia.com>
2025-09-22 09:44:58 -0700

    wip

--- ci/get_test_binary_dir.py
+++ ci/get_test_binary_dir.py
@@ -6,12 +6,11 @@ import os
 import sys
 
 root = os.path.dirname(numba_cuda.__file__)
-print("root:" + root)
 test_dir = os.path.join(
     root, "numba", "cuda", "tests", "test_binary_generation"
 )
 
 if os.path.isdir(test_dir):
-    print("test_dir exists:" + test_dir)
+    print(test_dir)
 else:
-    sys.exit(f"An error occured. {test_dir} does NOT exist.")
+    sys.exit(f"ERROR: '{test_dir}' does NOT exist.")
