d21220732fe6c8536136519adf99950abf2a7d64
Vijay Kandiah <vkandiah@nvidia.com>
2025-09-24 09:21:17 -0500

    [Refactor] Vendor in numba.core.config for future CUDA-specific changes (#450)
    This change vendors in `numba.core.config` for future CUDA-specific
    changes. Also updated a dangling reference to `numba.core.utils`.
    
    ---------
    
    Co-authored-by: brandon-b-miller <53796099+brandon-b-miller@users.noreply.github.com>

--- numba_cuda/numba/cuda/cuda_paths.py
+++ numba_cuda/numba/cuda/cuda_paths.py
@@ -6,9 +6,9 @@ import os
 from collections import namedtuple
 import platform
 import importlib.metadata
-from numba.core.config import IS_WIN32
+from numba.cuda.core.config import IS_WIN32
 from numba.cuda.misc.findlib import find_lib
-from numba import config
+from numba.cuda import config
 
 _env_path_tuple = namedtuple("_env_path_tuple", ["by", "info"])
 
