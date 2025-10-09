616ac5cff8ac02ad4f44a27f52a15aef7206def3
Atmn Patel <atmnp@nvidia.com>
2025-10-09 05:42:27 -0700

    [Refactor][NFC] Vendor-in vectorize decorators for future CUDA-specific changes
    This PR vendors in the vectorize and guvectorize decorators from Numba, which will allow for future CUDA-specific changes. The CPU-related functionality has been stripped so it no longer relies on a C extension.

--- numba_cuda/numba/cuda/tests/cudapy/test_vectorize.py
+++ numba_cuda/numba/cuda/tests/cudapy/test_vectorize.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from collections import namedtuple
 from itertools import product
-from numba import vectorize
+from numba.cuda import vectorize
 from numba import cuda, int32, float32, float64
 from numba.cuda.cudadrv.driver import CudaAPIError, driver
 from numba.cuda.testing import skip_on_cudasim
