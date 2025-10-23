068cfbd4ef0b66f5436d7f2cc3a95231f102d543
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2025-10-23 10:23:41 -0400

    chore: make name longer for clarity

--- numba_cuda/numba/cuda/api_util.py
+++ numba_cuda/numba/cuda/api_util.py
@@ -30,17 +30,17 @@ def prepare_shape_strides_dtype(shape, strides, dtype, order):
 
 @functools.cache
 def _fill_stride_by_order(shape, dtype, order):
-    nd = len(shape)
-    if nd == 0:
+    ndims = len(shape)
+    if ndims == 0:
         return ()
-    strides = [0] * nd
+    strides = [0] * ndims
     if order == "C":
         strides[-1] = dtype.itemsize
-        for d in reversed(range(nd - 1)):
+        for d in reversed(range(ndims - 1)):
             strides[d] = strides[d + 1] * shape[d + 1]
     elif order == "F":
         strides[0] = dtype.itemsize
-        for d in range(1, nd):
+        for d in range(1, ndims):
             strides[d] = strides[d - 1] * shape[d - 1]
     else:
         raise ValueError("must be either C/F order")
