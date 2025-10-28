c6ca4363c7150404c0e2fdfdbdf648aeaff34042
Atmn Patel <atmnp@nvidia.com>
2025-10-28 15:05:07 -0700

    [Refactor][NFC] Remove references to prange

--- numba_cuda/numba/cuda/typing/builtins.py
+++ numba_cuda/numba/cuda/typing/builtins.py
@@ -9,7 +9,6 @@ import operator
 from numba.cuda import types
 from numba.core import errors
 from numba import prange
-from numba.parfors.parfor import internal_prange
 
 from numba.cuda.typing.templates import (
     AttributeTemplate,
@@ -89,7 +88,6 @@ class Slice(ConcreteTemplate):
 
 @infer_global(range, typing_key=range)
 @infer_global(prange, typing_key=prange)
-@infer_global(internal_prange, typing_key=internal_prange)
 class Range(ConcreteTemplate):
     cases = [
         signature(types.range_state32_type, types.int32),
