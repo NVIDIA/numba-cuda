31af2441542020ae7f89be302b66166d42c497bd
Atmn Patel <atmnp@nvidia.com>
2025-10-30 11:05:56 -0700

    skip array reductions test on sim

--- numba_cuda/numba/cuda/tests/test_array_reductions.py
+++ numba_cuda/numba/cuda/tests/test_array_reductions.py
@@ -4,10 +4,12 @@ import numpy as np
 
 from numba.cuda.tests.support import TestCase, MemoryLeakMixin
 from numba import cuda
+from numba.cuda.testing import skip_on_cudasim
 from numba.cuda.misc.special import literal_unroll
 from numba.cuda import config
 
 
+@skip_on_cudasim
 class TestArrayReductions(MemoryLeakMixin, TestCase):
     """
     Test array reduction methods and functions such as .sum(), .max(), etc.
