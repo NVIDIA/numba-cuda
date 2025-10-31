5f0a4fa0723140ed97c10051ca18c1ab05446cfe
Graham Markall <gmarkall@nvidia.com>
2025-10-31 21:01:29 +0000

    Use HAS_NUMBA instead of importlib.util.find_spec() to check for Numba

--- numba_cuda/numba/cuda/initialize.py
+++ numba_cuda/numba/cuda/initialize.py
@@ -1,21 +1,24 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: BSD-2-Clause
-from importlib.util import find_spec
 
 
 def initialize_all():
     # Import models to register them with the data model manager
     import numba.cuda.models  # noqa: F401
 
-    if find_spec("numba"):
-        from numba.cuda.decorators import jit
-        from numba.cuda.dispatcher import CUDADispatcher
-        from numba.core.target_extension import (
-            target_registry,
-            dispatcher_registry,
-            jit_registry,
-        )
+    from numba.cuda import HAS_NUMBA
 
-        cuda_target = target_registry["cuda"]
-        jit_registry[cuda_target] = jit
-        dispatcher_registry[cuda_target] = CUDADispatcher
+    if not HAS_NUMBA:
+        return
+
+    from numba.cuda.decorators import jit
+    from numba.cuda.dispatcher import CUDADispatcher
+    from numba.core.target_extension import (
+        target_registry,
+        dispatcher_registry,
+        jit_registry,
+    )
+
+    cuda_target = target_registry["cuda"]
+    jit_registry[cuda_target] = jit
+    dispatcher_registry[cuda_target] = CUDADispatcher
