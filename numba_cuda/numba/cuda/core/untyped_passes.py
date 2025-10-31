2fbfa1312ca013a639c4500e192f620059cc8047
Atmn Patel <atmnp@nvidia.com>
2025-10-31 08:27:00 -0700

    Revert "Remove MakeFunctionToJitFunction pass, which implicitly uses njit, not a use case we intend to support"
    This reverts commit 0a60d27f5a0492c30b50393e3fc1fc395b308b35.

--- numba_cuda/numba/cuda/core/untyped_passes.py
+++ numba_cuda/numba/cuda/core/untyped_passes.py
@@ -35,6 +35,7 @@ from numba.cuda.core.ir_utils import (
     resolve_func_from_module,
     simplify_CFG,
     GuardException,
+    convert_code_obj_to_function,
     build_definitions,
     replace_var_names,
     get_name_var_table,
@@ -615,6 +616,72 @@ class PrintIRCFG(FunctionPass):
         self._ver += 1
         fir.render_dot(filename_prefix="v{}".format(self._ver)).render()
         return False
+
+
+@register_pass(mutates_CFG=True, analysis_only=False)
+class MakeFunctionToJitFunction(FunctionPass):
+    """
+    This swaps an ir.Expr.op == "make_function" i.e. a closure, for a compiled
+    function containing the closure body and puts it in ir.Global. It's a 1:1
+    statement value swap. `make_function` is already untyped
+    """
+
+    _name = "make_function_op_code_to_jit_function"
+
+    def __init__(self):
+        FunctionPass.__init__(self)
+
+    def run_pass(self, state):
+        from numba import njit
+
+        func_ir = state.func_ir
+        mutated = False
+        for idx, blk in func_ir.blocks.items():
+            for stmt in blk.body:
+                if isinstance(stmt, ir.Assign):
+                    if isinstance(stmt.value, ir.Expr):
+                        if stmt.value.op == "make_function":
+                            node = stmt.value
+                            getdef = func_ir.get_definition
+                            kw_default = getdef(node.defaults)
+                            ok = False
+                            if kw_default is None or isinstance(
+                                kw_default, ir.Const
+                            ):
+                                ok = True
+                            elif isinstance(kw_default, tuple):
+                                ok = all(
+                                    [
+                                        isinstance(getdef(x), ir.Const)
+                                        for x in kw_default
+                                    ]
+                                )
+                            elif isinstance(kw_default, ir.Expr):
+                                if kw_default.op != "build_tuple":
+                                    continue
+                                ok = all(
+                                    [
+                                        isinstance(getdef(x), ir.Const)
+                                        for x in kw_default.items
+                                    ]
+                                )
+                            if not ok:
+                                continue
+
+                            pyfunc = convert_code_obj_to_function(node, func_ir)
+                            func = njit()(pyfunc)
+                            new_node = ir.Global(
+                                node.code.co_name, func, stmt.loc
+                            )
+                            stmt.value = new_node
+                            mutated |= True
+
+        # if a change was made the del ordering is probably wrong, patch up
+        if mutated:
+            post_proc = postproc.PostProcessor(func_ir)
+            post_proc.run()
+
+        return mutated
 
 
 @register_pass(mutates_CFG=True, analysis_only=False)
