f9e0af7de3197ba95a2c569f40ce48de234e7230
Graham Markall <gmarkall@nvidia.com>
2025-11-12 14:33:21 +0000

    Add thirdparty tests of Awkward Array

---
+++ ci/test_thirdparty_awkward.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
+set -euo pipefail
+
+CUDA_VER_MAJOR_MINOR=${CUDA_VER%.*}
+AWKWARD_VERSION="2.8.10"
+
+rapids-logger "Install awkward and related libraries"
+
+pip install awkward==${AWKWARD_VERSION} cupy pyarrow pandas nox
+
+rapids-logger "Install wheel with test dependencies"
+package=$(realpath wheel/numba_cuda*.whl)
+echo "Package path: ${package}"
+python -m pip install \
+    "${package}" \
+    "cuda-python==${CUDA_VER_MAJOR_MINOR%.*}.*" \
+    "cuda-core==0.3.*" \
+    "nvidia-nvjitlink-cu12" \
+    --group test
+
+
+rapids-logger "Clone awkward repository"
+git clone --recursive https://github.com/sckit-hep/awkward.git
+pushd awkward
+git checkout v${AWKWARD_VERSION}
+
+rapids-logger "Generate awkward tests"
+nox -s prepare -- --tests
+
+rapids-logger "Check GPU usage"
+nvidia-smi
+
+rapids-logger "Show Numba system info"
+python -m numba --sysinfo
+
+rapids-logger "Run Awkward CUDA tests"
+python -m pytest -v -n auto tests-cuda tests-cuda-kernels tests-cuda-kernels-explicit
+
+popd
