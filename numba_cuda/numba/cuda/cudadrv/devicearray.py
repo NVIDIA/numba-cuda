89f1b45e798844ec6487135a4c992db96f6f12f1
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2025-12-05 13:41:34 -0500

    chore: make _numba_type_ an instance attribute to avoid repeated computation of type

--- numba_cuda/numba/cuda/cudadrv/devicearray.py
+++ numba_cuda/numba/cuda/cudadrv/devicearray.py
@@ -114,6 +114,7 @@ class DeviceNDArrayBase:
 
         self.gpu_data = gpu_data
         self.stream = stream
+        self._numba_type = None
 
     @property
     def __cuda_array_interface__(self):
@@ -179,17 +180,21 @@ class DeviceNDArrayBase:
         # of which will be 0, will not match those hardcoded in for 'C' or 'F'
         # layouts.
 
-        broadcast = 0 in self.strides and (self.size != 0)
+        if (numba_type := self._numba_type) is None:
+            broadcast = 0 in self.strides and (self.size != 0)
 
-        if self.flags["C_CONTIGUOUS"] and not broadcast:
-            layout = "C"
-        elif self.flags["F_CONTIGUOUS"] and not broadcast:
-            layout = "F"
-        else:
-            layout = "A"
+            if self.flags["C_CONTIGUOUS"] and not broadcast:
+                layout = "C"
+            elif self.flags["F_CONTIGUOUS"] and not broadcast:
+                layout = "F"
+            else:
+                layout = "A"
 
-        dtype = numpy_support.from_dtype(self.dtype)
-        return types.Array(dtype, self.ndim, layout)
+            dtype = numpy_support.from_dtype(self.dtype)
+            self._numba_type = numba_type = types.Array(
+                dtype, self.ndim, layout
+            )
+        return numba_type
 
     @property
     def device_ctypes_pointer(self):
